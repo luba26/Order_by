5d281bee68231b9a60d4d5ce8887b478180b4b24
Luba <80860762+luba26@users.noreply.github.com>
2023-10-29 18:33:05 -0300

    Creación de tarjetas

--- vistas/dashboards.php
+++ vistas/dashboards.php
@@ -19,6 +19,91 @@
 <!-- Main content -->
 <div class="content">
   <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-2">
+        <!-- etiqueta 1 dashboards -->
+        <div class="small-box bg-info">
+          <div class="inner">
+            <h4 id="totalIngresos">600</h4>
+            <p>Productos ingresados hoy</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-clipboard"></i>
+          </div>
+          <a style="cursor:pointer;" onclick="abrirUrl('vistas/inventario.php','content-wrapper');"
+            class="small-box-footer"> más info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <div class="col-lg-2">
+        <!-- etiqueta 2 dashboards -->
+        <div class="small-box bg-warning">
+          <div class="inner">
+            <h4 id="totalRetiros">500</h4>
+            <p>Productos retirados de bodega hoy</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-clipboard"></i>
+          </div>
+          <a style="cursor:pointer;" onclick="abrirUrl('vistas/inventario.php','content-wrapper');"
+            class="small-box-footer"> más info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <div class="col-lg-2">
+        <!-- etiqueta 3 dashboards -->
+        <div class="small-box bg-primary">
+          <div class="inner">
+            <h4 id="NivelStock">400</h4>
+            <p>Productos con bajo nivel de stock</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-clipboard"></i>
+          </div>
+          <a style="cursor:pointer;" onclick="abrirUrl('vistas/inventario.php','content-wrapper');"
+            class="small-box-footer"> más info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <div class="col-lg-2">
+        <!-- etiqueta 4 dashboards -->
+        <div class="small-box bg-secondary">
+          <div class="inner">
+            <h4 id="VencimientoProd">300</h4>
+            <p>Productos con más de 7 días en bodega</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-clipboard"></i>
+          </div>
+          <a style="cursor:pointer;" onclick="abrirUrl('vistas/inventario.php','content-wrapper');"
+            class="small-box-footer"> más info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <div class="col-lg-2">
+        <!-- etiqueta 5 dashboards -->
+        <div class="small-box bg-success">
+          <div class="inner">
+            <h4 id="TotalPrecioCompra">$35.000.000</h4>
+            <p>utiizados para compra de productos</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-cash"></i>
+          </div>
+          <a style="cursor:pointer;" onclick="abrirUrl('vistas/inventario.php','content-wrapper');"
+            class="small-box-footer"> más info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <div class="col-lg-2">
+        <!-- etiqueta 6 dashboards -->
+        <div class="small-box bg-danger">
+          <div class="text-center">
+            <h4>Visualizar inventario de bodega</h4>
+          </div>
+          <div class="icon">
+            <i class="ion ion-clipboard"></i>
+          </div>
+          <a style="cursor:pointer;" onclick="abrirUrl('vistas/inventario.php','content-wrapper');"
+            class="small-box-footer"> más info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+    </div>
   </div><!-- /.container-fluid -->
 </div>
 <!-- /.content -->
